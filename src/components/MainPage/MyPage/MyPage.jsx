e66e8afd233a4bb8f6b909fee31c6cb22a458149
Shurik <86realmadrid86@gmail.com>
2021-02-15 21:09:45 +0200

    add My posts component

--- src/components/MainPage/MyPage/MyPage.jsx
+++ src/components/MainPage/MyPage/MyPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './MyPage.module.css';
+import MyPosts from "./MyPosts/MyPage";
 
 const MyPage = () => {
     return (
@@ -8,7 +9,8 @@ const MyPage = () => {
             voluptate?
             Aliquid dignissimos enim excepturi illum laboriosam modi officia quisquam vel. Ad aperiam aspernatur beatae
             deserunt dignissimos doloremque dolorum eius eligendi eos error facilis id, iusto maxime molestias natus
-            nemo, nesciunt quis quisquam, saepe ut voluptatum?
+            nemo, nesciunt quis quisquam, saepe ut voluptatum?npm
+            <MyPosts />
         </div>
     );
 }
---
+++ src/components/MainPage/MyPage/MyPosts/MyPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './MyPage.module.css';
+
+const MyPosts = () => {
+    return (
+        <div className={classes.wrapper}>
+            may posts
+        </div>
+    );
+}
+
+export default MyPosts;
