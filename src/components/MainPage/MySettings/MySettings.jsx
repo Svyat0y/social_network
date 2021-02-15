a5a1e893ec71fa87aba2564a6ab09f24bc324dda
Shurik <86realmadrid86@gmail.com>
2021-02-15 21:02:00 +0200

    test push_1

--- src/components/MainPage/MySettings/MySettings.jsx
+++ src/components/MainPage/MySettings/MySettings.jsx
@@ -7,7 +7,7 @@ const MySettings = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur debitis expedita, officia quod
             ratione recusandae rem? Atque culpa cumque ducimus esse exercitationem fugiat id illo ipsa, ipsum iusto
             minus quaerat quas repellendus similique tempora vel voluptatem? Consectetur cum doloremque eaque et facere
-            nisi quae quaerat rem repudiandae velit! Necessitatibus, numquam? my settings ss
+            nisi quae quaerat rem repudiandae velit! Necessitatibus, numquam? my settings
         </div>
     );
 }
