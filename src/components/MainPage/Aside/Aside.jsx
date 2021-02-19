5f906e588cb75ed0c1c15d8ca44ae8c7562b9505
Shurik <86realmadrid86@gmail.com>
2021-02-19 22:59:23 +0200

    add Music component and small fixes

--- src/components/MainPage/Aside/Aside.jsx
+++ src/components/MainPage/Aside/Aside.jsx
@@ -8,6 +8,7 @@ const Aside = () => {
             <ul className={classes.nav}>
                 <li><NavLink exact to='/' activeClassName={classes.active}>My page</NavLink></li>
                 <li><NavLink to='/myMessages' activeClassName={classes.active}>My messages</NavLink></li>
+                <li><NavLink to='/myMusic' activeClassName={classes.active}>My Music</NavLink></li>
                 <li><NavLink to='/notifications' activeClassName={classes.active}>My notifications</NavLink></li>
                 <li><NavLink to='/mySettings' activeClassName={classes.active}>My settings</NavLink></li>
             </ul>
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './MainPage.module.css';
 import MyPage from './MyPage/MyPage';
 import MyMessages from './MyMessages/MyMessages';
+import Music from "./Music/Music";
 import Notifications from './Notifications/Notifications';
 import MySettings from './MySettings/MySettings';
 import {Route} from "react-router-dom";
@@ -12,6 +13,7 @@ const MainPage = () => {
 		<div className={classes.mainPage}>
 			<Route exact path='/' component={MyPage}/>
 			<Route path='/myMessages' component={MyMessages}/>
+			<Route path='/myMusic' component={Music}/>
 			<Route path='/notifications' component={Notifications}/>
 			<Route path='/mySettings' component={MySettings}/>
 		</div>
