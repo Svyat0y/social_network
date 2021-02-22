f033ed9bc112f8bf20210c16415b672ad5266c90
Shurik <86realmadrid86@gmail.com>
2021-02-22 20:55:42 +0200

    fix

--- src/components/MainPage/MyMessages/MyMessages.jsx
+++ src/components/MainPage/MyMessages/MyMessages.jsx
@@ -15,20 +15,36 @@ const MessageItem = (props) => {
 }
 
 const MyMessages = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Andy'},
+        {id: 2, name: 'Panda'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Vika'},
+        {id: 5, name: 'Dasha'},
+        {id: 6, name: 'Lusya'}
+    ]
+
+    let messagesData = [
+        {id: 0, message: 'Hi'},
+        {id: 1, message: 'How are you?'},
+        {id: 2, message: 'Are you here??'}
+    ]
+
     return (
         <div className={classes.wrapper}>
             <ul className={classes.dialog_items}>
-                <DialogItem name='Andy' id='1' />
-                <DialogItem name='Panda' id='2' />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
                 <DialogItem name='Sveta' id='3' />
                 <DialogItem name='Vika' id='4' />
                 <DialogItem name='Dasha' id='5' />
                 <DialogItem name='Lusya' id='6' />
             </ul>
             <div className={classes.dialog_messages}>
-                <MessageItem message='Hi' />
-                <MessageItem message='How are you??' />
-                <MessageItem message='Are you here' />
+                <MessageItem message={messagesData[0].message} />
+                <MessageItem message={messagesData[1].message} />
+                <MessageItem message={messagesData[2].message} />
             </div>
         </div>
     );
--- src/components/MainPage/MyPage/MyPost/MyPosts.jsx
+++ src/components/MainPage/MyPage/MyPost/MyPosts.jsx
@@ -3,6 +3,12 @@ import classes from './MyPosts.module.css';
 import MyPost from "./Post/MyPost";
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 0, message: 'Ho, how are you?', likeCount: 15},
+        {id: 1, message: "It's my first post", likeCount: 34},
+    ]
+
     return (
         <div className={classes.myPost_main_wr}>
             <span>My Post</span>
@@ -10,8 +16,8 @@ const MyPosts = () => {
                 <textarea name="post"/>
                 <button type="button">Add post</button>
             </form>
-            <MyPost likeCount={'15'} message={'Ho, how are you?'}/>
-            <MyPost likeCount={'34'} message={"It's my first post"}/>
+            <MyPost likeCount={postData[0].likeCount} message={postData[0].message}/>
+            <MyPost likeCount={postData[1].likeCount} message={postData[1].message}/>
         </div>
     );
 }
