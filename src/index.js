f455298ce5f1c1745c209d3db5cc4696652705c0
Shurik <86realmadrid86@gmail.com>
2021-03-30 19:23:10 +0300

    -small fixes: delete rerenderEntireTree in index.js

--- src/index.js
+++ src/index.js
@@ -6,27 +6,13 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-const rerenderEntireTree = (state) => {
-	ReactDOM.render(
-		<BrowserRouter>
-			<React.StrictMode>
-				<Provider store={store}>
-					<App
-						// state={state}
-						// dispatch={store.dispatch}
-						// store={store}
-					/>
-				</Provider>
-			</React.StrictMode>
-		</BrowserRouter>,
-		document.getElementById('root')
-	);
-}
-
-// отрисовка всего приложения
-rerenderEntireTree()
-
-// передаём в качестве аргумента нашу функцию отрисовки приложения
-store.subscribe(() => {
-	rerenderEntireTree()
-})
+ReactDOM.render(
+	<BrowserRouter>
+		<React.StrictMode>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</React.StrictMode>
+	</BrowserRouter>,
+	document.getElementById('root')
+);
