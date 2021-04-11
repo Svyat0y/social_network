4da2bc9f420f00715babcbff9b7bb50ed2942ef8
Shurik <86realmadrid86@gmail.com>
2021-04-11 15:33:24 +0300

    -call componentDidMount and put our request inside

--- src/components/MainPage/Users/Users.jsx
+++ src/components/MainPage/Users/Users.jsx
@@ -4,12 +4,9 @@ import * as axios from 'axios'
 
 // {
 // 	id: 0,
-// 		userImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6b7WENFquctsWy8WgMSHmFJbVyJXf0lQ4R2XRAQcd2WgOK2IIfFlLUFcAcsdnpA8S_Wg&usqp=CAU',
-// 	followed: false,
-// 	fullName: 'Andy',
-// 	status: 'i am a boss',
-// 	location: {country: 'Ukraine', city: 'Kiev'}
-// }
+// 		userImg:
+// 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6b7WENFquctsWy8WgMSHmFJbVyJXf0lQ4R2XRAQcd2WgOK2IIfFlLUFcAcsdnpA8S_Wg&usqp=CAU',
+// followed: false, fullName: 'Andy', status: 'i am a boss', location: {country: 'Ukraine', city: 'Kiev'} }
 
 // const Users = (props) => {
 //
@@ -28,11 +25,9 @@ import * as axios from 'axios'
 // }
 // export default Users;
 
-class Users extends React.Component  {
+class Users extends React.Component {
 
-	constructor(props) {
-		super(props);
-
+	componentDidMount() {
 		axios.get('https://social-network.samuraijs.com/api/1.0/users?page=1&count=5')
 			.then(response => this.props.setUsers(response.data.items))
 	}
