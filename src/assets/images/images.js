8131784cf7e0b7a48ab8be8bba8d1f8cae67a733
Alexander Pop <50931164+nekazsz@users.noreply.github.com>
2021-06-07 22:21:10 +0300

    Delete src directory

--- src/assets/images/images.js
+++
@@ -1,7 +0,0 @@
-import imgAvatarNotFound from './No_image_available.png'
-const images = {
-	imgAvatarNotFound: imgAvatarNotFound,
-	imgProfileNotFound: imgAvatarNotFound
-}
-
-export default images
--- src/hoc/withLazyLoad.js
+++
@@ -1,9 +0,0 @@
-import style from "../components/MainPage/MainPage.module.css";
-import Preloader from "../components/common/Preloader/Preloader";
-import React from "react";
-
-export const withLazyLoad = (Component) => {
-	return (props) => <React.Suspense fallback={<div className={style.preloader_wr}><Preloader /></div>}>
-		<Component {...props} />
-	</React.Suspense>
-}
